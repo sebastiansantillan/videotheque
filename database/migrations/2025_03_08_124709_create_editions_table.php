1c62f6e2b4aa675ffb30c1974c46af72e9d5c938
Sebastián Santillán <sebastian.santillan@gmail.com>
2025-03-08 10:05:28 -0300

    Se agregan los modelos Edition y Publisher

---
+++ database/migrations/2025_03_08_124709_create_editions_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('editions', function (Blueprint $table) {
+            $table->id();
+            $table->title('title', 100);
+            $table->string('releaseyear', 12);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('editions');
+    }
+};
